2bdd622ac097388fe0acea42cf82675598a0a93b
Julian David <julian.torregrosa@banlinea.com>
2017-08-30 15:59:13 -0500

    Create customization

--- banlinea.mobile.android.components/src/main/java/banlinea/mobile/components/otp/OtpView.kt
+++ banlinea.mobile.android.components/src/main/java/banlinea/mobile/components/otp/OtpView.kt
@@ -1,6 +1,10 @@
 package banlinea.mobile.components.otp
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.support.v7.widget.AppCompatEditText
 import android.text.InputFilter
 import android.text.InputType
 import android.util.AttributeSet
@@ -9,41 +13,94 @@ import android.view.View
 import android.widget.EditText
 import android.widget.LinearLayout
 import banlinea.mobile.components.R
+import java.lang.reflect.AccessibleObject.setAccessible
+import android.widget.TextView
+import android.graphics.drawable.Drawable
+import android.support.annotation.DrawableRes
+import android.util.TypedValue
+
 
 /**
  * Created by jdtor on 28.08.2017 for components.
  */
+
+@SuppressLint("RestrictedApi")
 class OtpView @JvmOverloads constructor(
         context: Context,
         attrs: AttributeSet? = null,
         defStyle: Int = 0,
         defStyleRes: Int = 0
 ) : LinearLayout(context, attrs, defStyle, defStyleRes) {
-    val mTexts : MutableList<EditText> = mutableListOf()
+
+    private val mTexts : MutableList<AppCompatEditText> = mutableListOf()
+
+    private var hint = "*"
+    private var size = 4
+    private var textColor : Int = Color.BLACK
+    private var hintColor : Int = Color.GRAY
+    private var tintColor : Int = Color.MAGENTA
+    private var textSize : Int = 0
+    private var spaceBetween : Int = 0
+    // private lateinit var drawable : Drawable
 
     init {
-        var hint: String = "*"
-        var total: Int = 4
+        // Create fields and configure layout
+        createFields()
+        createFocusActions()
+        layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+        orientation = HORIZONTAL
+
+        // Set options from attributes
         attrs?.let {
             val typedArray = context.obtainStyledAttributes(it, R.styleable.otp_attributes, 0, 0)
-            total = typedArray.getInt(R.styleable.otp_attributes_size, 4)
-            hint = typedArray.getString(R.styleable.otp_attributes_hint)
+            size = typedArray.getInt(R.styleable.otp_attributes_size, 4)
+            hint = typedArray.getString(R.styleable.otp_attributes_hint) ?: "*"
+            textColor = typedArray.getColor(R.styleable.otp_attributes_textColor, Color.BLACK)
+            hintColor = typedArray.getColor(R.styleable.otp_attributes_hintColor, Color.GRAY)
+            tintColor = typedArray.getColor(R.styleable.otp_attributes_tintColor, Color.MAGENTA)
+            // drawable = typedArray.getDrawable(R.styleable.otp_attributes_backgroundDrawable)
+            textSize = typedArray.getDimensionPixelSize(R.styleable.otp_attributes_textSize, 32)
+            spaceBetween = typedArray.getDimensionPixelSize(R.styleable.otp_attributes_spaceBetween, 32)
             typedArray.recycle()
-        }
 
-        var i : Int = 0
-        while(i < total){
-            val text = EditText(context)
+            for(text:AppCompatEditText in mTexts){
+                text.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 1f)
+                text.textAlignment = View.TEXT_ALIGNMENT_CENTER
+                text.inputType = InputType.TYPE_CLASS_NUMBER
+                text.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(1))
+                text.hint = hint
+                text.setTextColor(textColor)
+                text.setHintTextColor(hintColor)
+                val textParams = text.layoutParams as LinearLayout.LayoutParams
+                    textParams.marginEnd = spaceBetween/2
+                    textParams.marginStart = spaceBetween/2
+
+                /*
+                if (drawable != null)
+                    text.setBackgroundDrawable(drawable)
+                else {*/
+                    val tintColorStateList : ColorStateList = ColorStateList.valueOf(tintColor)
+                    text.supportBackgroundTintList = tintColorStateList
+                //}
+                text.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize.toFloat())
+            }
+        }
+    }
+    private fun createFields(){
+        var i = 0
+        while(i < size){
+            val text = AppCompatEditText(context)
             addView(text)
-            text.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 1f)
-            text.textAlignment = View.TEXT_ALIGNMENT_CENTER
-            text.inputType = InputType.TYPE_CLASS_NUMBER
-            text.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(1))
-            text.hint = hint
             mTexts.add(text)
             i++
         }
-        for(text:EditText in mTexts){
+    }
+
+    /**
+     * Need mTexts before
+     */
+    private fun createFocusActions(){
+        for(text:AppCompatEditText in mTexts){
             text.setOnKeyListener(object: OnKeyListener{
                 override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
                     if(event?.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_DEL){
@@ -57,7 +114,5 @@ class OtpView @JvmOverloads constructor(
                 }
             })
         }
-        layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
-        orientation = HORIZONTAL
     }
 }
