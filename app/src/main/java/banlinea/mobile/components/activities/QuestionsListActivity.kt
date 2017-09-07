304ed29d2e1166b518b244aa3d6d6ad683682a45
jdtorregrosas <jdtorregrosas@gmail.com>
2017-09-07 13:24:27 -0500

    Create unstable question component

--- app/src/main/java/banlinea/mobile/components/activities/QuestionsListActivity.kt
+++ app/src/main/java/banlinea/mobile/components/activities/QuestionsListActivity.kt
@@ -3,7 +3,12 @@ package banlinea.mobile.components.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import banlinea.mobile.components.R
+import banlinea.mobile.components.questionsList.Question
+import banlinea.mobile.components.questionsList.models.Answer
+import banlinea.mobile.components.questionsList.models.QuestionType
+import kotlinx.android.synthetic.main.activity_questions_list.*
 import kotlinx.android.synthetic.main.toolbar.*
+import org.jetbrains.anko.alert
 
 class QuestionsListActivity : AppCompatActivity() {
 
@@ -15,5 +20,14 @@ class QuestionsListActivity : AppCompatActivity() {
         supportActionBar?.title = "Questions List Component"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         toolbar.setNavigationOnClickListener{ _ -> onBackPressed()}
+        val question1 = Question(this, QuestionType.CLOSED, listOf("Cuál es tu fruta ciudad 2"),
+                listOf<Answer>(Answer(1, "Pogotá"), Answer(2, "Metrellín")))
+        testFrame.addView(question1.getView())
+        testFrame.addView(Question(this, QuestionType.OPEN, listOf("Cuál es tu fruta ciudad 2")).getView())
+        testFrame.addView(Question(this, QuestionType.OPEN, listOf("Cuál es tu edad refrutera?")).getView())
+        testFrame.addView(Question(this, QuestionType.CLOSED, listOf("Cuál es tu fruta ciudad 2"),
+                listOf<Answer>(Answer(1, "Pogotá"), Answer(2, "Metrellín"))).getView())
+
+        testButton.setOnClickListener{_ -> alert(question1.getAnswer().value){}.show()}
     }
 }
--- banlinea.mobile.android.components/src/main/java/banlinea/mobile/components/questionsList/Question.kt
+++ banlinea.mobile.android.components/src/main/java/banlinea/mobile/components/questionsList/Question.kt
@@ -1,11 +1,69 @@
 package banlinea.mobile.components.questionsList
 
+import android.R
+import android.content.Context
+import android.support.v7.widget.AppCompatButton
+import android.support.v7.widget.AppCompatEditText
+import android.support.v7.widget.AppCompatTextView
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.view.View
+import android.widget.*
 import banlinea.mobile.components.questionsList.models.Answer
 import banlinea.mobile.components.questionsList.models.QuestionType
 
 /**
  * Created by julian on 7/09/17.
  */
-class Question(type: QuestionType, labels: List<String>, answers: List<Answer>) {
+class Question(context: Context, type: QuestionType, labels: List<String>, answers: List<Answer> = listOf()) {
 
+    private val linearLayout = LinearLayout(context)
+    private lateinit var finalAnswer : Answer
+
+    init {
+        linearLayout.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        linearLayout.orientation = LinearLayout.VERTICAL
+        val label = AppCompatTextView(context)
+        label.text = labels[0]
+        linearLayout.addView(label)
+
+        when(type){
+            QuestionType.OPEN -> {
+                val answerEditText = AppCompatEditText(context)
+                linearLayout.addView(answerEditText)
+                answerEditText.inputType = InputType.TYPE_CLASS_TEXT
+                answerEditText.addTextChangedListener(object: TextWatcher{
+                    override fun afterTextChanged(p0: Editable?) {}
+                    override fun beforeTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+                    override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                        finalAnswer = Answer(1, text.toString())
+                    }
+                })
+            }
+            QuestionType.CLOSED -> {
+                val spinner = Spinner(context)
+                spinner.adapter = ArrayAdapter(context, R.layout.simple_spinner_dropdown_item, answers.map { it.value })
+                spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+                    override fun onNothingSelected(p0: AdapterView<*>?) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                    override fun onItemSelected(parentView: AdapterView<*>?, selectedItemView: View?, position: Int, id: Long) {
+                        finalAnswer = answers[position]
+                    }
+
+                }
+                linearLayout.addView(spinner)
+            }
+        }
+    }
+
+    fun getView() : LinearLayout{
+        return linearLayout
+    }
+
+    fun getAnswer() : Answer {
+        return finalAnswer
+    }
 }
