e837e269eb9d4d7e315392601fc0c10e3162048e
Julian David <julian.torregrosa@banlinea.com>
2017-09-15 16:48:00 -0500

    Create showNextAfterCompletion attribute

--- components/src/main/java/com/jdtorregrosas/components/questionsList/QuestionsListView.kt
+++ components/src/main/java/com/jdtorregrosas/components/questionsList/QuestionsListView.kt
@@ -2,8 +2,10 @@ package com.jdtorregrosas.components.questionsList
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.ScrollView
+import banlinea.mobile.components.R
 
 /**
  * Created by jdtor on 09.09.2017 for components.
@@ -14,16 +16,25 @@ class QuestionsListView @JvmOverloads constructor(
         defStyle: Int = 0,
         defStyleRes: Int = 0
 ) : ScrollView(context, attrs, defStyle, defStyleRes) {
+
+    private var showNextAfterCompletion = false
     init {
         layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+
+        attrs?.let {
+            val typedArray = context.obtainStyledAttributes(it, R.styleable.QuestionsListView, 0, 0)
+            showNextAfterCompletion = typedArray.getBoolean(R.styleable.QuestionsListView_showNextAfterCompletion, false)
+            typedArray.recycle()
+        }
     }
     fun setQuestions(questions : List<Question>){
         val linearLayout = LinearLayout(context)
         linearLayout.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
         linearLayout.orientation = LinearLayout.VERTICAL
         linearLayout.setPadding(40,40,40,40)
-        for(question in questions){
-            linearLayout.addView(question.getView())
+        for(i in 0 until questions.size){
+            linearLayout.addView(questions[i].getView())
+            if(showNextAfterCompletion && i!=0) questions[i].getView().visibility = View.GONE
         }
         this.addView(linearLayout)
     }
